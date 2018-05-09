4b5f9073703265036214e92756cfec0514392d96
gsc <gsc.mobile.dev@gmail.com>
2018-05-10 00:02:33 +0200

    Add SearchView

--- app/src/main/java/com/gsc/recipes/ui/recipes/RecipeListActivity.java
+++ app/src/main/java/com/gsc/recipes/ui/recipes/RecipeListActivity.java
@@ -3,6 +3,7 @@ package com.gsc.recipes.ui.recipes;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.gsc.recipes.R;
 
@@ -15,4 +16,11 @@ public class RecipeListActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_recipe_list, menu);
+        return true;
+    }
+
 }
