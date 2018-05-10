c00a3e0cc3fbd659afefc0d91aaffdfefac64a10
gsc <gsc.mobile.dev@gmail.com>
2018-05-10 20:27:24 +0200

    Add query text listener to SearchView

--- app/src/main/java/com/gsc/recipes/ui/recipes/RecipeListActivity.java
+++ app/src/main/java/com/gsc/recipes/ui/recipes/RecipeListActivity.java
@@ -4,10 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.SearchView;
 
 import com.gsc.recipes.R;
 
-public class RecipeListActivity extends AppCompatActivity {
+public class RecipeListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +21,23 @@ public class RecipeListActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_recipe_list, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setOnQueryTextListener(this);
+
         return true;
     }
 
+
+    //region OnQueryTextListener
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+    //endregion
 }
